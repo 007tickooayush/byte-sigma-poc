e92d3f4e3af8b4e7733afadff195c4fac36f5607
007tickooayush <ayush.tickoo292000@gmail.com>
2024-05-10 10:00:48 +0530

    front end init

--- _frontend/src/App.jsx
+++ _frontend/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [images, setImages] = useState([]);
+	const page = 1; // change this to change the page
+	const limit = 10; // change this to change the number of files per page
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		const arrayBufferToBase64 = (buffer) => {
+			const binary = new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), '');
+			return window.btoa(binary);
+		};
+		// const arrayBufferToBase64 = (buffer) => {
+		// 	var binary = '';
+		// 	var bytes = [].slice.call(new Uint8Array(buffer));
+		// 	bytes.forEach((b) => binary += String.fromCharCode(b));
+		// 	return window.btoa(binary);
+		// };
+
+		fetch(`${import.meta.env.VITE_API_URL}/uploads?page=${page}&limit=${limit}`, {
+			method: 'GET',
+			// headers: {
+			// 	'Authorization': `Bearer ${yourToken}`
+			// }
+		})
+			.then(data => data.json())
+			.then(data => {
+				console.log('data :>> ', data);
+				data.forEach(image => {
+					let base64Flag = 'data:image/jpeg;base64,';
+					let imageStr = arrayBufferToBase64(image.buffer.data);
+					setImages([...images, { path: base64Flag + imageStr }])
+				});
+			})
+			.catch(error => console.error(error));
+	}, [page, limit]);
+
+	// rest of your code...
+
+	return (
+		<div>
+			{images.map((image, index) => (
+				<img key={index} src={image.path} alt={`Image ${index}`} />
+			))}
+		</div>
+
+	)
 }
 
 export default App
